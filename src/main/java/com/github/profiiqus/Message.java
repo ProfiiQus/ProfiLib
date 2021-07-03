17a60db5ad2ba9ec38fdc623ad303e4a9687edd5
ProfiiQus <profiiqus@gmail.com>
2021-07-03 22:45:13 +0200

    Add fromPath method

--- src/main/java/com/github/profiiqus/Message.java
+++ src/main/java/com/github/profiiqus/Message.java
@@ -1,6 +1,9 @@
 package com.github.profiiqus;
 
 import com.github.profiiqus.utils.Formatter;
+import com.google.common.base.Strings;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.*;
 
@@ -31,5 +34,25 @@ public class Message {
         for(int i = 0; i < lines.size()-1; i++) {
             lines.set(i, Formatter.colorize(lines.get(i)));
         }
+    }
+
+    public static Message fromPath(FileConfiguration config, String path) throws NullPointerException, InvalidConfigurationException {
+        if(Strings.isNullOrEmpty(path)) {
+            throw new NullPointerException("Message path is null/empty");
+        }
+        if(config.getString(path) == null) {
+            throw new NullPointerException("Message on " + path + "is empty");
+        }
+
+        Object msg = config.get(path);
+        if(msg instanceof String) {
+            return new Message((String) msg);
+        }
+
+        if(msg instanceof List) {
+            return new Message((ArrayList<String>) msg);
+        }
+
+        throw new InvalidConfigurationException("Invalid message type on: " + path);
     }
 }
